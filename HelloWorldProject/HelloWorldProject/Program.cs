06d5969c63e3ebecdfd652ead62efe852a745589
Evy Avila <384036@colum.edu>
2023-12-17 20:18:13 -0600

    Adding project

---
+++ HelloWorldProject/HelloWorldProject/Program.cs
@@ -0,0 +1,29 @@
+﻿//By Evy Avila
+
+using NumberLibrary;
+using System.Security.Cryptography;
+
+namespace HelloWorldProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int rand = RandomNumberGenerator.GetInt32(200);
+            
+            var input = rand;
+            
+            if (input.GetType() != typeof(int))
+            {
+                input = (int)Math.Round((decimal)input);
+            }
+
+            Console.WriteLine($"Is {input} an even number? " + Number.IsNumberEven((int)input));
+           
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ HelloWorldProject/NumberLibrary/Number.cs
@@ -0,0 +1,25 @@
+﻿namespace NumberLibrary
+{
+    public static class Number
+    {
+        public static bool IsNumberEven(this int? number)
+        {
+
+
+            if (number == null)
+            {
+                return false;
+            }
+            else
+            {
+                if(number % 2 == 0)
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
